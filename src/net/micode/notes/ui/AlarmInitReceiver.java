ec86f8d3b54284ceb7ee879645178b3f67efdb08
starsviolet <124482786+starsviolet@users.noreply.github.com>
2023-03-22 00:17:28 +0800

    Update AlarmInitReceiver.java

--- src/net/micode/notes/ui/AlarmInitReceiver.java
+++ src/net/micode/notes/ui/AlarmInitReceiver.java
@@ -30,7 +30,7 @@ import net.micode.notes.data.Notes.NoteColumns;
 
 public class AlarmInitReceiver extends BroadcastReceiver {
 
-    private static final String [] PROJECTION = new String [] {
+    private static final String [] PROJECTION = new String [] {    //对数据库的操作，调用标签ID和闹钟时间
         NoteColumns.ID,
         NoteColumns.ALERTED_DATE
     };
@@ -40,14 +40,14 @@ public class AlarmInitReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        long currentDate = System.currentTimeMillis();
+        long currentDate = System.currentTimeMillis();//System.currentTimeMillis()产生一个当前的毫秒，这个毫秒其实就是自1970年1月1日0时起的毫秒数
         Cursor c = context.getContentResolver().query(Notes.CONTENT_NOTE_URI,
                 PROJECTION,
                 NoteColumns.ALERTED_DATE + ">? AND " + NoteColumns.TYPE + "=" + Notes.TYPE_NOTE,
-                new String[] { String.valueOf(currentDate) },
-                null);
+                new String[] { String.valueOf(currentDate) },  //将long变量currentDate转化为字符串
+                null);//Cursor在这里的作用是通过查找数据库中的标签内容，找到和当前系统时间相等的标签
 
-        if (c != null) {
+        if (c != null) {//根据数据库里的闹钟时间创建一个闹钟机制
             if (c.moveToFirst()) {
                 do {
                     long alertDate = c.getLong(COLUMN_ALERTED_DATE);
